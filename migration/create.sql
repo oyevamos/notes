0597f8bd89b7b2b73a50053aa99d03e8ad538657
oyevamos <deadhead2520@gmail.com>
2024-04-11 16:51:03 +0300

    feat: sql table corrected, NOT NULL added

--- migration/create.sql
+++ migration/create.sql
@@ -1,7 +1,7 @@
 CREATE TABLE notes (
-                       id SERIAL PRIMARY KEY,
-                       user_id INT,
-                       header VARCHAR,
+                       id SERIAL NOT NULL PRIMARY KEY,
+                       user_id INT NOT NULL,
+                       header VARCHAR NOT NULL,
                        content VARCHAR,
                        date_created TIMESTAMP,
                        date_modified TIMESTAMP
